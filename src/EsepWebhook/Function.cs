57feeed0f3007de6969216bf6a59c1a415900194
Faizan Haque <faizanjhaque@gmail.com>
2025-04-11 16:16:58 -0400

    new commit

--- src/EsepWebhook/Function.cs
+++ src/EsepWebhook/Function.cs
@@ -1,39 +1,46 @@
-using System.Text;
 using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+using System.Text;
+using System.Net.Http;
+using System.IO;
 using Newtonsoft.Json;
 
-// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
-
 namespace EsepWebhook;
 
 public class Function
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
-    public string FunctionHandler(object input, ILambdaContext context)
+    private static readonly HttpClient client = new HttpClient();
+
+    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
     {
-        context.Logger.LogInformation($"FunctionHandler received: {input}");
-
-        dynamic json = JsonConvert.DeserializeObject<dynamic>(input.ToString());
-
-    
-        string payload = $"{{'text':'Issue Created: {json.issue.html_url}'}}";
-
-        var client = new HttpClient();
-        var webRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+        try
         {
-            Content = new StringContent(payload, Encoding.UTF8, "application/json")
-        };
-    
-        var response = client.Send(webRequest);
-        using var reader = new StreamReader(response.Content.ReadAsStream());
+            context.Logger.LogInformation($"Received body: {request.Body}");
+            dynamic json = JsonConvert.DeserializeObject(request.Body);
+            string issueUrl = json?.issue?.html_url;
             
-        return reader.ReadToEnd();
+            var slackMessage = JsonConvert.SerializeObject(new { text = $"Issue Created: {issueUrl}" });
+            var slackRequest = new HttpRequestMessage(HttpMethod.Post, Environment.GetEnvironmentVariable("SLACK_URL"))
+            {
+                Content = new StringContent(slackMessage, Encoding.UTF8, "application/json")
+            };
+
+            var response = client.Send(slackRequest);
+
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 200,
+                Body = "Posted to Slack successfully"
+            };
+        }
+        catch (Exception ex)
+        {
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 500,
+                Body = $"Exception: {ex.Message}"
+            };
+        }
     }
 }
